0080f718086f18e584008831cf23361f51045775
chrisvalmas <aljbs2012@yahoo.com>
2017-03-09 10:09:25 -0500

    Update Process.h

--- Project1/Process.h
+++ Project1/Process.h
@@ -13,27 +13,31 @@ public:
 
     // getters
     char getID() const { return pid; }
-	int getArrivalTime() const{ return arrivalTime; }
-	int getIOTime() const{ return ioTime; }
-	int getBurstTime() const{ return burstTime; }
+    int getArrivalTime() const{ return arrivalTime; }
+    int getIOTime() const{ return ioTime; }
+    int getBurstTime() const{ return burstTime; }
     int getRemaining() const { return timeRemaining; }
     int getWaitTime() const { return waitTime; }
-	int getEndIO() const { return endIO; }
+    int getEndIO() const { return endIO; }
+    int getStartTime() const { return startTime; }
     int getBursts() const { return nBurst-currBurst; }
+    int getEndBurst() const { return endBurst; } 
 
     // setters
     void setRemaining(int t) { timeRemaining = t; }
+    void setSRTEndBurst(int t) {endBurst = t + timeRemaining; }
     void setEndBurst(int t) { endBurst = t + burstTime; }
-	void setEndIO(int t) { endIO = t + ioTime; }
+    void setEndIO(int t) { endIO = t + ioTime; }
     void setArrivalTime(int t) { arrivalTime = t; }
-	void clearWaitTime() { waitTime = 0; }
+    void clearWaitTime() { waitTime = 0; }
+    void setStartTime(int t) { startTime = t; }
 
     // incrementors
     void incCurrBurst() { currBurst++; }
     void incWaitTime() { waitTime++; }
  
     // tests
-	bool arrived(int t) const{ return t == arrivalTime; }
+    bool arrived(int t) const{ return t == arrivalTime; }
     bool burstDone(int t) { return t == endBurst; }
     bool ioDone(int t) { return t == endIO; }
     bool completed();   // test whether process has more cpu bursts to perform
@@ -47,6 +51,7 @@ private:
     int burstTime;
     int nBurst;
     int ioTime;
+    int startTime;
  
     int currBurst;
     int timeRemaining;
